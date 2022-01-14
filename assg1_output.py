fc46120d98e04b1e8968816f69d230b0d86ba0e8
sparsh1802 <50405879+sparsh1802@users.noreply.github.com>
2022-01-14 11:06:45 +0530

    Add files via upload

---
+++ assg1_output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\dell\Desktop\pyton\assg1.py ================
+Enter number 1: 55
+Enter number 2: 36
+Enter number 3: 24
+The average of numbers is:  38.333333333333336
+
+
+Enter your gross income: 50000
+Enter number of dependents: 4
+Your annual tax is:  5600.0
+
+
+Enter your name: Sparsh Aggarwal
+Enter your sid: 21104048
+Enter your gender: M
+Enter your course name: EE
+Enter your CGPA: 9
+[21104048, 'Sparsh Aggarwal', 'M', 'EE', '9']
+
+
+Enter marks of 1st student 27
+Enter marks of 2nd student 25
+Enter marks of 3rd student 24
+Enter marks of 4th student 20
+Enter marks of 5th student 30
+[20, 24, 25, 27, 30]
+
+
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+After Deleting 4th item, new list is: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Answer for part B:  ['Red', 'Green', 'White', 'purple', 'Yellow']
